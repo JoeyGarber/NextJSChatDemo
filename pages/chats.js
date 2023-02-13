8d6b6455e6b3cbdd10bdd8d6ef28bd25a9d51e38
joeygarber <joseph.z.garber@gmail.com>
2023-02-12 20:42:43 -0500

    Initial commit

--- pages/chats.js
+++ pages/chats.js
@@ -1,5 +1,56 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from 'react';
+
+import { Context } from '../context';
+
+import { useRouter } from 'next/router';
+// This is for dynamic importing
+// The chat components use Quill, maintained by Salesforce
+// It doesn't natively support Next.js, so we have to import it like this
+import dynamic from 'next/dynamic'
+
+const ChatEngine = dynamic(() => 
+  import('react-chat-engine').then((module) => module.ChatEngine)
+)
+
+const MessageFormSocial = dynamic(() => 
+  import('react-chat-engine').then((module) => module.MessageFormSocial)
+)
 
 export default function Chats() {
-  return <div className="background">chats</div>;
+  const { username, secret } = useContext(Context)
+  const [showChat, setShowChat] = useState(false)
+  const router = useRouter()
+
+  // NextJS runs in the same project on the client and server side
+  // So we need to run a check to see if document exists
+  // Because Quill requires the document
+  useEffect(() => {
+    if (typeof document !== null) {
+      setShowChat(true)
+    }
+  })
+
+  useEffect(() => {
+    if (username.length === 0 || secret.length === 0) {
+      router.push('/')
+    }
+  })
+
+  if (!showChat) return (
+    <div />
+  )
+
+  return (
+    <div className="background">
+      <div className="shadow">
+        <ChatEngine
+          height='calc(100vh - 200px)'
+          projectID='f15a3635-59d0-4ea2-ac1d-0db48aaa1954'
+          userName={username}
+          userSecret={secret}
+          renderNewMessageForm={() => <MessageFormSocial />}
+        />
+      </div>
+    </div>
+  );
 }
